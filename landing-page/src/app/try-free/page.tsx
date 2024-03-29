87f86e7e3117b115bb45b97ce38da5e429ce33de
Sarah Azouvi <sarahazouvi@sarahs-macbook-air.home>
2024-03-29 21:59:56 +0100

    changed 'try free' page to have two buttons instead of drop-down menu

--- landing-page/src/app/try-free/page.tsx
+++ landing-page/src/app/try-free/page.tsx
@@ -1,44 +1,35 @@
-"use client"; // This is a client component 👈🏽
-
-import React, { useState } from "react";
+// pages/try-free.tsx
+import React from "react";
 import Link from "next/link";
 import SubscribeBanner from "@/Components/SubscribeBanner";
 
 const TryFree: React.FC = () => {
   const mailListUrl =
     "https://docs.google.com/forms/d/e/1FAIpQLScDUUi878bC2WxMHkMmNm5_T-CZNuHK1zQeYoihjYlIlQhnBg/viewform?usp=sf_link";
-  const [selectedModel, setSelectedModel] = useState("");
-
-  const handleModelChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedModel(event.target.value);
-  };
 
   return (
     <div className="min-h-screen flex flex-col">
       <SubscribeBanner mailListUrl={mailListUrl} />
       <div className="flex-grow flex justify-center items-center flex-col">
-        <h1 className="text-4xl font-bold mb-4">Choose model</h1>
-        <select
-          value={selectedModel}
-          onChange={handleModelChange}
-          className="mb-8 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:bg-white focus:border-black border-black text-black"
-          //className="block appearance-none w-full bg-white border border-black text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-black"
-        >
-          <option value="" disabled>
-            Select a model
-          </option>
-          <option value="model1">Model 1</option>
-          <option value="model2">Model 2</option>
-          <option value="model3">Model 3</option>
-        </select>
-        {selectedModel && (
+        <h1 className="text-4xl font-bold mb-8">Choose a Model</h1>
+        <div className="flex gap-4 mb-8">
           <Link
-            href={`/models/${selectedModel}`}
-            className="bg-gray-800 text-white px-6 py-3 rounded-md text-lg hover:bg-gray-700"
+            href="/models/model1"
+            className="bg-gray-800 text-white px-6 py-3 rounded-md  hover:bg-gray-700 "
           >
-            View Model Details
+            <div className="text-center">
+              <span> Model 1</span>
+            </div>
+            <span className="text-sm">text-to-image</span>
           </Link>
-        )}
+          <Link
+            href="/models/model2"
+            className="bg-gray-800 text-white px-6 py-3 rounded-md  hover:bg-gray-700"
+          >
+            <div className="text-center">Model 2</div>
+            <span className="text-sm">text-to-img</span>
+          </Link>
+        </div>
         <div className="text-center">
           {/* Add your try-free page content here */}
         </div>
