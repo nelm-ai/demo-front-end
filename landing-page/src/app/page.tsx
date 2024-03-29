a5f0185dc08d649eca8e2c527013f0d5276f6bf5
Sarah Azouvi <sarahazouvi@sarahs-macbook-air.home>
2024-03-29 11:39:41 +0100

    moved the subscribe button to the top right and used tailwind instead of css modules

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import styles from "../styles/Home.module.css";
-import TryFree from "./try-free";
+import Link from "next/link";
 
 const Home: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <nav>
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLScDUUi878bC2WxMHkMmNm5_T-CZNuHK1zQeYoihjYlIlQhnBg/viewform?usp=sf_link"
-            className={styles.subscribeBtn}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Subscribe to NELM Mailing List
-          </a>
-        </nav>
-      </header>
-      <main className={styles.main}>
-        <h1 className={styles.title}>NELM</h1>
-        <p className={styles.tagline}>Bring Dark Compute to Light</p>
-        <a href="/try-free" className={styles.tryFreeBtn}>
-          Try for Free
+    <div className="min-h-screen flex flex-col">
+      <header className=" py-4 px-6 flex justify-end">
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScDUUi878bC2WxMHkMmNm5_T-CZNuHK1zQeYoihjYlIlQhnBg/viewform?usp=sf_link"
+          className="text-white hover:text-gray-300"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Subscribe to NELM Mailing List
         </a>
+      </header>
+      <main className="flex-grow flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold mb-2">NELM</h1>
+        <p className="text-lg mb-8">Bring Dark Compute to Light</p>
+        <Link
+          href="/try-free"
+          className="bg-gray-800 text-white px-6 py-3 rounded-md text-lg hover:bg-gray-700"
+        >
+          Try for Free
+        </Link>
       </main>
     </div>
   );
