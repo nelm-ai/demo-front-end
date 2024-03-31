921e97540299c078a8134e46818a123be1e05bcc
Sarah Azouvi <sarahazouvi@sarahs-macbook-air.home>
2024-03-31 20:02:38 +0200

    changed color of buttons when we are on that page already

--- landing-page/src/Components/ModelButton.tsx
+++ landing-page/src/Components/ModelButton.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 const ModelButtons = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const getButtonStyle = (path) => {
+    return pathname === path
+      ? "bg-gray-600 text-white px-4 py-2 rounded mr-4 hover:bg-gray-500"
+      : "bg-gray-800 text-white px-4 py-2 rounded mr-4 hover:bg-gray-700";
+  };
+
   return (
     <div className="flex justify-center mt-4">
       <Link href="/models/model1">
-        <button className="bg-gray-800 text-white px-4 py-2 rounded mr-4 hover:bg-gray-700">
-          Model 1
-        </button>
+        <button className={getButtonStyle("/models/model1")}>Model 1</button>
       </Link>
       <Link href="/models/model2">
-        <button className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700">
-          Model 2
-        </button>
+        <button className={getButtonStyle("/models/model2")}>Model 2</button>
       </Link>
     </div>
   );
