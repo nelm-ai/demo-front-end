2657adcf49cf6652a179e95ebab7bb59c9edd0a0
Sarah Azouvi <sarahazouvi@sarahs-macbook-air.home>
2024-03-31 20:16:25 +0200

    disabled clocking on the model on which we already are

--- landing-page/src/Components/ModelButton.tsx
+++ landing-page/src/Components/ModelButton.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const ModelButtons = () => {
   const pathname = usePathname();
-  const router = useRouter();
-
-  const getButtonStyle = (path) => {
-    return pathname === path
-      ? "bg-gray-600 text-white px-4 py-2 rounded mr-4 hover:bg-gray-500"
-      : "bg-gray-800 text-white px-4 py-2 rounded mr-4 hover:bg-gray-700";
-  };
 
   return (
     <div className="flex justify-center mt-4">
       <Link href="/models/model1">
-        <button className={getButtonStyle("/models/model1")}>Model 1</button>
+        <button
+          className={`bg-gray-800 text-white px-4 py-2 rounded mr-4 hover:bg-gray-700 ${
+            pathname === "/models/model1" ? "cursor-not-allowed opacity-50" : ""
+          }`}
+          disabled={pathname === "/models/model1"}
+        >
+          Model 1
+        </button>
       </Link>
       <Link href="/models/model2">
-        <button className={getButtonStyle("/models/model2")}>Model 2</button>
+        <button
+          className={`bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 ${
+            pathname === "/models/model2" ? "cursor-not-allowed opacity-50" : ""
+          }`}
+          disabled={pathname === "/models/model2"}
+        >
+          Model 2
+        </button>
       </Link>
     </div>
   );
